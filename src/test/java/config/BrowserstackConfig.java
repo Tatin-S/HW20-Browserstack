55e164011c78eae444b982530d78b8e6159ef5d8
Tatiana Stulova <vitadbnf@mail.ru>
2024-07-27 23:29:58 +0300

    change config

--- src/test/java/config/BrowserstackConfig.java
+++ src/test/java/config/BrowserstackConfig.java
@@ -5,38 +5,33 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.FIRST)
 @Config.Sources({
         "classpath:${env}.properties",
-      //  "classpath:android.properties"
 })
 public interface BrowserstackConfig extends Config {
     @Key("browserstackApp")
-    @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
+  //  @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
     String browserstackApp();
 
-/*    @Key("browserstackPlatformName")
-    @DefaultValue("android")
-    String browserstackplatformName();*/
-
     @Key("browserstackDevice")
-    @DefaultValue("Google Pixel 3")
+ //   @DefaultValue("Google Pixel 3")
     String browserstackDevice();
 
     @Key("browserstackPlatform")
-    @DefaultValue("9.0")
+ //   @DefaultValue("9.0")
     String browserstackPlatform();
 
     @Key("browserstackProject")
-    @DefaultValue("First Java Project")
+ //   @DefaultValue("First Java Project")
     String browserstackProject();
 
     @Key("browserstackBuild")
-    @DefaultValue("browserstack-build-1")
+ //   @DefaultValue("browserstack-build-1")
     String browserstackBuild();
 
     @Key("browserstackName")
-    @DefaultValue("first_test")
+  //  @DefaultValue("first_test")
     String browserstackName();
 
     @Key("browserstackUrl")
-    @DefaultValue("https://hub.browserstack.com/wd/hub")
+  //  @DefaultValue("https://hub.browserstack.com/wd/hub")
     String browserstackUrl();
 }

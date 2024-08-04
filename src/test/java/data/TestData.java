d9beceafcc3d205b0d005fa71544ff3064e1693d
Tatiana Stulova <vitadbnf@mail.ru>
2024-08-04 21:37:19 +0300

    change WikiTests

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -3,5 +3,5 @@ package data;
 public class TestData {
     public String noResults = "No results";
     public String valueSearch = "BrowserStack";
-    public String valueEmptySearch = "EmptySearch";
+    public String valueEmptySearch = "errortest";
 }

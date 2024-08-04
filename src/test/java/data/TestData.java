b9f2c0355225ecbd6f1672f4b86bc90201bb0822
Tatiana Stulova <vitadbnf@mail.ru>
2024-08-04 21:06:27 +0300

    change config

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -3,5 +3,5 @@ package data;
 public class TestData {
     public String errorMessage = "An error occurred";
     public String valueSearch = "BrowserStack";
-    public String valueSearchError = "Browserstack";
+    public String valueSearchError = "errortest";
 }
